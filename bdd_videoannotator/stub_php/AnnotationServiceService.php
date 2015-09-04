9e4eaa32c1c4d37a9cfd3504bf3f1508edac7b84
shell88 <stefan88hell@gmail.com>
2015-09-04 12:42:39 +0200

    Actualized standalone server to 0.1-beta.3

--- bdd_videoannotator/stub_php/AnnotationServiceService.php
+++ bdd_videoannotator/stub_php/AnnotationServiceService.php
@@ -14,9 +14,9 @@ class AnnotationServiceService extends \SoapClient {
 
 	const WSDL_FILE = "tmp/META-INF/wsdl/AnnotationServiceService.wsdl";
 	private $classmap = array(
-			'stepResult' => '\bdd_videoannotator\stub_php\stepResult',
 			'stringArray' => '\bdd_videoannotator\stub_php\stringArray',
 			'stringArrayArray' => '\bdd_videoannotator\stub_php\stringArrayArray',
+			'stepResult' => '\bdd_videoannotator\stub_php\stepResult',
 			);
 
 	public function __construct($wsdl = null, $options = array()) {
@@ -29,6 +29,50 @@ class AnnotationServiceService extends \SoapClient {
 			$this->__setSoapHeaders($options['headers']);
 		}
 		parent::__construct($wsdl ?: self::WSDL_FILE, $options);
+	}
+
+	/**
+	 *  
+	 *
+	 * @param $path
+	 * @return void
+	 */
+	public function changeOutputDirectory($path) {
+		return $this->__soapCall('changeOutputDirectory', array($path), array(
+						'uri' => 'http://service.bddvideoannotator.shell88.github.com/',
+						'soapaction' => ''
+					)
+			);
+	}
+
+	/**
+	 *  
+	 *
+	 * @param $result
+	 * @return void
+	 */
+	public function addResultToBufferStep($result) {
+		return $this->__soapCall('addResultToBufferStep', array($result), array(
+						'uri' => 'http://service.bddvideoannotator.shell88.github.com/',
+						'soapaction' => ''
+					)
+			);
+	}
+
+	/**
+	 *  
+	 *
+	 * @param $steptext
+	 * @param $datatable
+	 * @param $result
+	 * @return void
+	 */
+	public function addStepWithResult($steptext, $datatable, $result) {
+		return $this->__soapCall('addStepWithResult', array($steptext, $datatable, $result), array(
+						'uri' => 'http://service.bddvideoannotator.shell88.github.com/',
+						'soapaction' => ''
+					)
+			);
 	}
 
 	/**
@@ -82,50 +126,6 @@ class AnnotationServiceService extends \SoapClient {
 	 */
 	public function addStepToBuffer($steptext, $datatable) {
 		return $this->__soapCall('addStepToBuffer', array($steptext, $datatable), array(
-						'uri' => 'http://service.bddvideoannotator.shell88.github.com/',
-						'soapaction' => ''
-					)
-			);
-	}
-
-	/**
-	 *  
-	 *
-	 * @param $path
-	 * @return void
-	 */
-	public function changeOutputDirectory($path) {
-		return $this->__soapCall('changeOutputDirectory', array($path), array(
-						'uri' => 'http://service.bddvideoannotator.shell88.github.com/',
-						'soapaction' => ''
-					)
-			);
-	}
-
-	/**
-	 *  
-	 *
-	 * @param $result
-	 * @return void
-	 */
-	public function addResultToBufferStep($result) {
-		return $this->__soapCall('addResultToBufferStep', array($result), array(
-						'uri' => 'http://service.bddvideoannotator.shell88.github.com/',
-						'soapaction' => ''
-					)
-			);
-	}
-
-	/**
-	 *  
-	 *
-	 * @param $steptext
-	 * @param $datatable
-	 * @param $result
-	 * @return void
-	 */
-	public function addStepWithResult($steptext, $datatable, $result) {
-		return $this->__soapCall('addStepWithResult', array($steptext, $datatable, $result), array(
 						'uri' => 'http://service.bddvideoannotator.shell88.github.com/',
 						'soapaction' => ''
 					)
